d3d057e8c365be80ccdf1f82e0f44d652bbd05de
Ky Leggiero <KyLeggiero@Gmail.com>
2023-05-18 15:20:08 -0600

    Some improvements to Gender and Pronouns

--- Sources/GenderTools/Gender.swift
+++ Sources/GenderTools/Gender.swift
@@ -2,16 +2,30 @@
 //  Gender.swift
 //  
 //
-//  Created by Ky Leggiero on 2/16/22.
+//  Created by Ky Leggiero on 2022-02-16.
+//  CC-0 License
 //
 
 import Foundation
 
 
 
+/// A person's gender
 public struct Gender {
-    let genderName: String
-    let pronouns: Pronouns
+    
+    /// The user-readable name of this gender
+    public let genderName: String
+    
+    /// The pronouns associated with this gender, so you may refer to the user properly
+    public let pronouns: [Pronouns]
+}
+
+
+
+public extension Gender {
+    init(genderName: String, pronouns: Pronouns? = nil) {
+        self.init(genderName: genderName, pronouns: pronouns.map { [$0] } ?? [])
+    }
 }
 
 
@@ -19,10 +33,29 @@ public struct Gender {
 // MARK: - Default instances
 
 public extension Gender {
+    
+    /// The most common feminine gender: Woman
     static let woman = Gender(genderName: "Woman", pronouns: .sheHer)
+    
+    /// The most common masculine gender: Man
     static let man = Gender(genderName: "Man", pronouns: .heHim)
     
+    
+    /// Both an umbrella term for any gender which isn't strictly masculine nor feminine, and also (to some) its own gender
     static let nonBinary = Gender(genderName: "Non-Binary", pronouns: .theyThem)
+    
+    /// Both an umbrella term for a lack of gender, and also (to some) its own gender
     static let agender = Gender(genderName: "Agender", pronouns: .theyThem)
+    
+    /// A gender which is a flux of feminine genders
     static let genderfae = Gender(genderName: "Genderfae", pronouns: .faeFaer)
+    
+    /// A gender which is a flux of masculine genders
+    static let genderfaun = Gender(genderName: "Genderfaun", pronouns: .heHim)
 }
+
+
+
+// MARK: - Conformances
+
+extension Gender: Hashable {}
--- Sources/GenderTools/Pronouns.swift
+++ Sources/GenderTools/Pronouns.swift
@@ -2,7 +2,8 @@
 //  Pronouns.swift
 //  
 //
-//  Created by Ky Leggiero on 2/16/22.
+//  Created by Ky Leggiero on 2022-02-16.
+//  CC-0 License
 //
 
 import Foundation
@@ -10,22 +11,42 @@ import Foundation
 
 
 /// Represents a full set of English pronoun conjugationss, for use in text about someone. Intended for use as singular 3rd-person pronouns, but can be used for any plurality & perspective
-public struct Pronouns {
+public enum Pronouns {
+
+    /// The typical set of 5 conjugations of pronouns used in English, such as `they/them/their/theirs/themself`
+    ///
+    /// - Parameters:
+    ///   - subjective: The subjective form of these pronouns, like `they`. For example:  "_ loves cake"
+    ///   - objective: The objective form of these pronouns, like `them`. For example:  "Morgan will give _ some"
+    ///   - possessiveDeterminer: The possessive determiner form of these pronouns, like `their`. For example:  "That's _ favorite"
+    ///   - possessive: The possessive form of these pronouns, like `theirs`. For example:  "That spot is _"
+    ///   - reflexive: The reflexive form of these pronouns, like `themself`. For example:  "More cake all to _"
+    case englishConjugated(
+        subjective: String,
+        objective: String,
+        possessiveDeterminer: String,
+        possessive: String,
+        reflexive: String)
     
-    /// The subjective form of these pronouns, like `they`. For example:  "_ loves cake"
-    public let subjective: String
-    
-    /// The objective form of these pronouns, like `them`. For example:  "Morgan will give _ some"
-    public let objective: String
-    
-    /// The possessive determiner form of these pronouns, like `their`. For example:  "That's _ favorite"
-    public let possessiveDeterminer: String
-    
-    /// The possessive form of these pronouns, like `theirs`. For example:  "That spot is _"
-    public let possessive: String
-    
-    /// The reflexive form of these pronouns, like `themself`. For example:  "More cake all to _"
-    public let reflexive: String
+    /// Any pronouns (user has no preference)
+    case any
+}
+
+
+
+public extension Pronouns {
+    init(subjective: String,
+         objective: String,
+         possessiveDeterminer: String,
+         possessive: String,
+         reflexive: String) {
+        self = .englishConjugated(
+            subjective: subjective,
+            objective: objective,
+            possessiveDeterminer: possessiveDeterminer,
+            possessive: possessive,
+            reflexive: reflexive)
+    }
 }
 
 
@@ -49,19 +70,85 @@ public extension Pronouns {
 
 
 
+extension Pronouns: CaseIterable {
+    
+    /// All common pronoun sets and many uncommon ones. `.any` is excluded from this list.
+    public static var allCases: [Pronouns] = [
+        theyThem,
+        sheHer,
+        heHim,
+        itIts,
+        faeFaer,
+        eyEm,
+        eEm,
+        sieHir,
+        xeXem,
+    ]
+    
+    /// A randomly-chosen set of pronouns. This is different every time you call it
+    static var random: Pronouns { allCases.randomElement() ?? .theyThem }
+}
+
+
+
 // MARK: - Conveniences
+
+public extension Pronouns {
+    var englishConjugations: EnglishConjugations {
+        switch self {
+        case .englishConjugated(let subjective, let objective, let possessiveDeterminer, let possessive, let reflexive):
+            return EnglishConjugations(
+                subjective: subjective,
+                objective: objective,
+                possessiveDeterminer: possessiveDeterminer,
+                possessive: possessive,
+                reflexive: reflexive
+            )
+            
+        case .any:
+            return Self.random.englishConjugations
+        }
+    }
+    
+    
+    
+    typealias EnglishConjugations = (
+        subjective: String,
+        objective: String,
+        possessiveDeterminer: String,
+        possessive: String,
+        reflexive: String
+    )
+}
 
 extension Pronouns: CustomStringConvertible {
     
     public var description: String {
-        let first = subjective
+        let conjugations = self.englishConjugations
+        let first = conjugations.subjective
         let second =
-            objective != first ? objective :
-            possessiveDeterminer != first ? possessiveDeterminer :
-            possessive != first ? possessive :
-            reflexive != first ? reflexive :
-            first
+        conjugations.objective != first
+            ? conjugations.objective
+            : conjugations.possessiveDeterminer != first
+                ? conjugations.possessiveDeterminer
+                : conjugations.possessive != first
+                    ? conjugations.possessive
+                    : conjugations.reflexive != first
+                        ? conjugations.reflexive
+                        : first
         
         return "\(first)/\(second)"
     }
 }
+
+
+
+// MARK: - `Hashable`
+
+extension Pronouns: Hashable {}
+
+
+
+// MARK: - `Codable`
+
+extension Pronouns: Codable {}
